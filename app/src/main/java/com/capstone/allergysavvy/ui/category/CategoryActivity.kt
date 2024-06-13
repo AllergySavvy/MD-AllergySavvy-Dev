6fe16a3f007ad6efafac8bd9fb1946cb9c312429
Airi <airiagustus82@gmail.com>
2024-06-14 00:19:44 +0700

    Add starter binding for CategoryActivity

--- app/src/main/java/com/capstone/allergysavvy/ui/category/CategoryActivity.kt
+++ app/src/main/java/com/capstone/allergysavvy/ui/category/CategoryActivity.kt
@@ -6,12 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.capstone.allergysavvy.R
+import com.capstone.allergysavvy.databinding.ActivityCategoryBinding
 
 class CategoryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCategoryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityCategoryBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_category)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
