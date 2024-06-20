4507aa457807f4561c5000b63e6ccc6cb90212a0
Airi <airiagustus82@gmail.com>
2024-06-20 12:55:22 +0700

    Add Api Service for use the response

--- app/src/main/java/com/capstone/allergysavvy/data/retrofit/ApiService.kt
+++ app/src/main/java/com/capstone/allergysavvy/data/retrofit/ApiService.kt
@@ -1,9 +1,21 @@
 package com.capstone.allergysavvy.data.retrofit
 
+import com.capstone.allergysavvy.data.response.FoodDetailResponse
+import com.capstone.allergysavvy.data.response.GetFoodRandomResponse
+import com.capstone.allergysavvy.data.response.LoginResponse
+import com.capstone.allergysavvy.data.response.RegisterResponse
 import com.capstone.allergysavvy.data.response.SearchListResponse
+import com.capstone.allergysavvy.data.response.ShowAllIngredientResponse
+import com.capstone.allergysavvy.data.response.ShowRandomIngredientResponse
+import com.capstone.allergysavvy.data.response.UserDataResponse
 import com.capstone.allergysavvy.data.response.VideoDetailsResponse
 import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
@@ -23,4 +35,43 @@ interface ApiService {
         @Query("id") id: String,
         @Query("key") apiKey: String
     ): Call<VideoDetailsResponse>
+
+    @FormUrlEncoded
+    @POST("register")
+    suspend fun registerUser(
+        @Field("email") email: String,
+        @Field("password") password: String,
+        @Field("username") username: String,
+    ): RegisterResponse
+
+    @FormUrlEncoded
+    @POST("login")
+    suspend fun loginUser(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): LoginResponse
+
+    @GET("ingredient")
+    suspend fun getIngredient(): ShowAllIngredientResponse
+
+    @GET("ingredient/random")
+    suspend fun getRandomIngredient(): ShowRandomIngredientResponse
+
+    @GET("user")
+    suspend fun getUserData(): UserDataResponse
+
+    @POST("user/allergies")
+    suspend fun userAllergies(
+        @Part("user_allergies") userAllergies: String
+    ): UserDataResponse
+
+    @GET("food/{id}")
+    suspend fun getFoodRecipesDetail(
+        @Path("id") id: String
+    ): FoodDetailResponse
+
+    @GET("food/random")
+    suspend fun getFoodRandom(): GetFoodRandomResponse
+
+
 }
