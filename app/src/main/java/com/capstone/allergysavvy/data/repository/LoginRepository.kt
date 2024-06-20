d9202984ef6a3c1d2e8aa5086144015ae07984d7
Airi <airiagustus82@gmail.com>
2024-06-20 13:00:12 +0700

    Add repository for login and injection for login repository

---
+++ app/src/main/java/com/capstone/allergysavvy/data/repository/LoginRepository.kt
@@ -0,0 +1,24 @@
+package com.capstone.allergysavvy.data.repository
+
+import com.capstone.allergysavvy.data.response.LoginResponse
+import com.capstone.allergysavvy.data.retrofit.ApiService
+
+class LoginRepository(
+    private val apiService: ApiService
+) {
+    suspend fun login(email: String, password: String): LoginResponse {
+        return apiService.loginUser(email, password)
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: LoginRepository? = null
+
+        fun getInstance(
+            apiService: ApiService
+        ): LoginRepository =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: LoginRepository(apiService)
+            }.also { INSTANCE = it }
+    }
+}
--- app/src/main/java/com/capstone/allergysavvy/di/Injection.kt
+++ app/src/main/java/com/capstone/allergysavvy/di/Injection.kt
@@ -1,12 +1,23 @@
 package com.capstone.allergysavvy.di
 
+import android.content.Context
+import com.capstone.allergysavvy.data.local.pref.UserPreference
+import com.capstone.allergysavvy.data.local.pref.dataStore
+import com.capstone.allergysavvy.data.repository.LoginRepository
 import com.capstone.allergysavvy.data.repository.VideoRepository
 import com.capstone.allergysavvy.data.retrofit.ApiConfig
+import kotlinx.coroutines.runBlocking
 
 object Injection {
     fun videoRepository(): VideoRepository {
         val apiService = ApiConfig.getApiServiceYoutube()
         return VideoRepository.getInstance(apiService)
+    }
 
+    fun loginRepository(context: Context): LoginRepository {
+        val pref = UserPreference.getInstance(context.dataStore)
+        val user = runBlocking { pref.getUserToken() }
+        val apiService = ApiConfig.getApiService(user)
+        return LoginRepository.getInstance(apiService)
     }
 }
