d614eb759df593f62b3b28c7800e237e6482b1e6
Airi <airiagustus82@gmail.com>
2024-06-20 12:13:37 +0700

    Change methode for video fragment (cancel paging 3, Reason: Quota limit for Youtube API)

--- app/src/main/java/com/capstone/allergysavvy/ui/main/fragment/video/VideoFragment.kt
+++ app/src/main/java/com/capstone/allergysavvy/ui/main/fragment/video/VideoFragment.kt
@@ -6,17 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.asFlow
-import androidx.lifecycle.lifecycleScope
-import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.capstone.allergysavvy.data.Result
 import com.capstone.allergysavvy.databinding.FragmentVideoBinding
 import com.capstone.allergysavvy.di.Injection
-import com.capstone.allergysavvy.ui.adapter.LoadingStateAdapter
 import com.capstone.allergysavvy.ui.adapter.VideoAdapter
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 
 class VideoFragment : Fragment() {
     private var _binding: FragmentVideoBinding? = null
@@ -25,7 +20,6 @@ class VideoFragment : Fragment() {
     private val videoViewModel: VideoViewModel by viewModels {
         VideoViewModelFactory.getInstance(videoRepository)
     }
-    private lateinit var videoAdapter: VideoAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,73 +31,67 @@ class VideoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        checkVideo()
         setupRecyclerView()
-        setupProgressBarAndErrorMassageVideo()
         searchVideo()
     }
 
+    private fun checkVideo() {
+        if (videoViewModel.videos.value == null) {
+            videoViewModel.searchVideo("How To Make Healthy Food Recipes")
+        }
+    }
 
     private fun setupRecyclerView() {
-        videoAdapter = VideoAdapter()
+        val videoAdapter = VideoAdapter()
         binding.rvVideoYoutube.apply {
-            adapter = videoAdapter.withLoadStateFooter(
-                footer = LoadingStateAdapter {
-                    videoAdapter.retry()
-                }
-            )
+            adapter = videoAdapter
             layoutManager = LinearLayoutManager(context)
-        }
-
-        lifecycleScope.launch {
-            videoViewModel.searchVideo("How To Make Healthy Food Recipes").asFlow()
-                .collectLatest { pagingData ->
-                    videoAdapter.submitData(pagingData)
-                }
         }
     }
 
     private fun searchVideo() {
-        binding.edSearchRecipeVideo.setOnClickListener {
-            val searchQuery =
-                "${binding.edSearchRecipeVideo.text.toString()} + tutorial food recipes"
-            if (searchQuery.isNotEmpty()) {
-                lifecycleScope.launch {
-                    videoViewModel.searchVideo(searchQuery).asFlow().collectLatest { pagingData ->
-                        videoAdapter.submitData(pagingData)
+        videoViewModel.videos.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is Result.Loading -> {
+                    binding.progressBarVideo.visibility = View.VISIBLE
+                }
+
+                is Result.Error -> {
+                    binding.progressBarVideo.visibility = View.GONE
+                    Snackbar.make(
+                        binding.root,
+                        "Error finding video: ${result.error}",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                    with(binding) {
+                        ivErrorIconFragmentVideo.visibility = View.VISIBLE
+                        tvOopsFragmentVideo.visibility = View.VISIBLE
+                        tvErrorMessageFragmentVideo.visibility = View.VISIBLE
                     }
                 }
-            } else {
-                Snackbar.make(binding.root, "Please enter a search query", Snackbar.LENGTH_SHORT)
-                    .show()
-            }
-        }
-    }
 
-    private fun setupProgressBarAndErrorMassageVideo() {
-        videoAdapter.addLoadStateListener { loadState ->
-            binding.progressBarVideo.visibility =
-                if (loadState.source.refresh is LoadState.Loading) {
-                    View.VISIBLE
-                } else {
-                    View.GONE
+                is Result.Success -> {
+                    binding.progressBarVideo.visibility = View.GONE
+                    val videoAdapter = binding.rvVideoYoutube.adapter as VideoAdapter
+                    videoAdapter.submitList(result.data)
                 }
-
-            val errorState = loadState.source.append as? LoadState.Error
-                ?: loadState.source.prepend as? LoadState.Error
-                ?: loadState.source.refresh as? LoadState.Error
-                ?: loadState.append as? LoadState.Error
-                ?: loadState.prepend as? LoadState.Error
-
-            if (errorState != null) {
-                binding.ivErrorImageFragmentVideo.visibility = View.VISIBLE
-                binding.tvOopsFragmentVideo.visibility = View.VISIBLE
-                binding.tvSoryMessageVideoFragment.visibility = View.VISIBLE
-            } else {
-                binding.ivErrorImageFragmentVideo.visibility = View.GONE
-                binding.tvOopsFragmentVideo.visibility = View.GONE
-                binding.tvSoryMessageVideoFragment.visibility = View.GONE
             }
         }
+
+        binding.edSearchRecipeVideo.setOnClickListener {
+            val searchQuery = "${binding.edSearchRecipeVideo.text.toString()} + tutorial recipes"
+            if (searchQuery.isNotEmpty()) {
+                videoViewModel.searchVideo(searchQuery)
+            } else {
+                Snackbar.make(
+                    binding.root,
+                    "Please enter a recipe name",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+        }
+
     }
 
     override fun onDestroyView() {
