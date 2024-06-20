1871836ae1eb71cf1d63d158a25df760ed364e7a
Airi <airiagustus82@gmail.com>
2024-06-21 02:48:55 +0700

    Add function for ingredient fragment

--- app/src/main/java/com/capstone/allergysavvy/ui/main/fragment/ingredient/IngredientFragment.kt
+++ app/src/main/java/com/capstone/allergysavvy/ui/main/fragment/ingredient/IngredientFragment.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.GridLayoutManager
 import com.capstone.allergysavvy.databinding.FragmentIngredientBinding
+import com.capstone.allergysavvy.ui.adapter.IngredientAdapter
+import com.capstone.allergysavvy.ui.adapter.LoadingStateAdapter
+import com.google.android.material.snackbar.Snackbar
 
 class IngredientFragment : Fragment() {
     private var _binding: FragmentIngredientBinding? = null
@@ -25,6 +31,39 @@ class IngredientFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val ingredientAdapter = IngredientAdapter()
+
+        binding.rvIngredient.layoutManager = GridLayoutManager(requireActivity(), 3)
+        binding.rvIngredient.adapter = ingredientAdapter.withLoadStateFooter(
+            footer = LoadingStateAdapter {
+                ingredientAdapter.retry()
+            }
+        )
+
+        ingredientViewModel.ingredient.observe(viewLifecycleOwner) {
+            ingredientAdapter.submitData(lifecycle, it)
+        }
+
+        ingredientAdapter.addLoadStateListener {
+            binding.progressBarIngredient.isVisible = it.source.refresh is LoadState.Loading
+            if (it.source.refresh is LoadState.Error) {
+                val error = (it.source.refresh as LoadState.Error).error
+                showSnackBar(error.message.toString())
+            }
+        }
+    }
+
+    private fun showSnackBar(message: String) {
+        Snackbar.make(
+            requireView(),
+            message,
+            Snackbar.LENGTH_SHORT
+        ).show()
     }
 
     override fun onDestroyView() {
